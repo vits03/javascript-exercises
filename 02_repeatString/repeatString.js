d3ffea34b6d6bdfb1242c75fb374e3d3445cd357
vits03 <101546379+vits03@users.noreply.github.com>
2022-06-09 23:01:36 +0400

    completed exercise 1 to 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let i=0;  
+    let rstring=""
+    if (num<0){
+        return "ERROR"
+    }
+    for (;i<num;i++){
+      rstring+=string
+    }
+    return rstring;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(sentence) {
+    let final="";
+    for (let i=sentence.length-1;i>=0;i--){
+         final+=sentence[i];
+ }
+ return final;
 };
 
 // Do not edit below this line
